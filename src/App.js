75b92b186fde7e4bd6a7c18201511b00841485ef
Ruaraidh <rca.plummer@gmail.com>
2021-02-17 16:41:27 +0000

    #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    clicker: 0
+  }
+
+  increaseClicker = () => {
+    this.setState({
+      clicker: this.state.clicker +1
+    })
+  }  
+  decreaseClicker = () => {
+    this.setState({
+      clicker: this.state.clicker -1
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="number" value={this.state.clicker}></input>
+        <button onClick={this.increaseClicker}>Increase</button>
+        <button onClick={this.decreaseClicker}>Decrease</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
